ce93f50fd21011d3e006f15098157ad257c73716
akrigline <a.krigline@gmail.com>
2021-10-31 14:28:39 -0400

    disable item level inputs to prevent double update

--- scripts/module.js
+++ scripts/module.js
@@ -1,3 +1,6 @@
+/**
+ * Overall class containing the setup logic
+ */
 class LinkItemResource5e {
   static MODULE_NAME = "link-item-resource-5e";
   static MODULE_TITLE = "Link Item and Resource DnD5e";
@@ -12,14 +15,33 @@ class LinkItemResource5e {
     loadTemplates(Object.values(this.TEMPLATES));
 
     libWrapper.register(LinkItemResource5e.MODULE_NAME, 'CONFIG.Actor.documentClass.prototype.prepareData', LinkItemResource5eActor.prepareDerivedResources, "WRAPPER");
-  }
 
+    Hooks.on("preUpdateItem", LinkItemResource5eItem.handlePreItemUpdate);
+    Hooks.on('renderItemSheet', LinkItemResource5eItemSheet.handleRender);
+    Hooks.on('preUpdateActor', LinkItemResource5eActor.handlePreUpdateActor);
+    Hooks.on('renderActorSheet5eCharacter', LinkItemResource5eActorSheet.handleActorSheetRender);
+  }
+}
+
+Hooks.on("setup", LinkItemResource5e.setup);
+
+/**
+ * Contains all resource-linking logic related to the Item document
+ */
+class LinkItemResource5eItem {
+  /**
+   * preUpdateItem hook callback
+   * Updates the parent actor's flags detailing which resources are overriden when
+   * the resource-link flag on the item is changed
+   * @param {*} item 
+   * @param {*} updateData 
+   * @returns 
+   */
   static handlePreItemUpdate = (item, updateData) => {
-    const flagPath = `flags.${this.MODULE_NAME}.resource-link`;
+    const flagPath = `flags.${LinkItemResource5e.MODULE_NAME}.resource-link`;
     const actor = item.actor;
-    const currentValue = item.getFlag(this.MODULE_NAME, 'resource-link');
+    const currentValue = item.getFlag(LinkItemResource5e.MODULE_NAME, 'resource-link');
     const newValue = foundry.utils.getProperty(updateData, flagPath);
-    debugger;
 
     if (!foundry.utils.hasProperty(updateData, flagPath)) {
       return;
@@ -30,9 +52,8 @@ class LinkItemResource5e {
     }
 
     // either update the actor or we have to hijack the prepareData somehow
-    const currentOverrides = actor.getFlag(this.MODULE_NAME, 'resource-overrides');
+    const currentOverrides = actor.getFlag(LinkItemResource5e.MODULE_NAME, 'resource-overrides');
 
-    let actorUpdates = {};
     const newFlags = {
       ...currentOverrides,
     }
@@ -50,21 +71,26 @@ class LinkItemResource5e {
       newFlags[newValue] = item.id;
     }
 
-    actor.update({
-      [`flags.${this.MODULE_NAME}.resource-overrides`]: newFlags,
-    });
+    actor.setFlag(LinkItemResource5e.MODULE_NAME, 'resource-overrides', newFlags);
   }
-
 }
 
-Hooks.on("setup", LinkItemResource5e.setup);
-Hooks.on("preUpdateItem", LinkItemResource5e.handlePreItemUpdate);
 
+/**
+ * Contains all resource-linking logic related to the ItemSheet
+ */
 class LinkItemResource5eItemSheet {
   static capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
+  /**
+   * renderItemSheet Hook callback
+   * Injects the input for selecting a resouce to override
+   * @param {*} itemSheet 
+   * @param {*} html 
+   * @returns 
+   */
   static handleRender = async (itemSheet, html) => {
     const item = itemSheet.item;
     const actor = item.actor;
@@ -93,10 +119,19 @@ class LinkItemResource5eItemSheet {
   }
 }
 
-Hooks.on('renderItemSheet', LinkItemResource5eItemSheet.handleRender);
 
+/**
+ * Contains all resource-linking logic related to the Actor Document
+ */
 class LinkItemResource5eActor {
-  // has to run after all the owned items have run `prepareFinalAttributes` so items have numerical charges
+  /**
+   * `Actor5e.prepareData` WRAPPER
+   * Overrides the flagged resources with item details.
+   * Has to run after all the owned items have run `prepareFinalAttributes` so items have numerical charges
+   * @param {*} wrapped 
+   * @param  {...any} args 
+   * @returns 
+   */
   static prepareDerivedResources(wrapped, ...args) {
     wrapped(...args);
 
@@ -127,6 +162,7 @@ class LinkItemResource5eActor {
   }
 
   /**
+   * preUpdateActor hook callback
    * Mutates `updateData` to correctly update the item instead of the actor resource in cases
    * where the resource is overridden.
    * @param {*} actor 
@@ -135,7 +171,7 @@ class LinkItemResource5eActor {
    */
   static handlePreUpdateActor = (actor, updateData) => {
     const currentOverrides = actor.getFlag(LinkItemResource5e.MODULE_NAME, 'resource-overrides');
-    
+
     // get any updates to the actor's resources
     const resourceUpdates = foundry.utils.getProperty(updateData, `data.resources`);
     if (!currentOverrides || !resourceUpdates) {
@@ -150,7 +186,6 @@ class LinkItemResource5eActor {
     if (!updatesToOverriddenResources.length) {
       return;
     }
-    debugger;
 
     // construct item updates based on the updateData
     const itemUpdates = updatesToOverriddenResources
@@ -169,10 +204,11 @@ class LinkItemResource5eActor {
   }
 }
 
-Hooks.on('preUpdateActor', LinkItemResource5eActor.handlePreUpdateActor);
 
+/**
+ * Contains all resource-linking logic related to the ActorSheet
+ */
 class LinkItemResource5eActorSheet {
-
   // from 5e sheet resources plus module
   static sheetResources = [
     "primary",
@@ -202,8 +238,13 @@ class LinkItemResource5eActorSheet {
     return this.sheetResources.indexOf(resourceName);
   }
 
-  // has to run after all the owned items have run `prepareFinalAttributes` so items have numerical charges
-  static disableDerivedFields = (actorSheet, html) => {
+  /**
+   * renderActorSheet hook callback
+   * @param {*} actorSheet 
+   * @param {*} html 
+   * @returns 
+   */
+  static handleActorSheetRender = (actorSheet, html) => {
     const actor = actorSheet.actor;
     const resourceOverrides = actor.getFlag(LinkItemResource5e.MODULE_NAME, 'resource-overrides');
 
@@ -211,17 +252,44 @@ class LinkItemResource5eActorSheet {
       return;
     }
 
-    const indexesToDisable = Object.keys(resourceOverrides).map(this.getIndexFromResourceName);
+    this.disableDerivedResourceFields(resourceOverrides, html);
+    this.disableOverriddenItemFields(resourceOverrides, html);
+  }
 
+  /**
+   * Disables all inputs in an overriden resource fieldset except `value`.
+   * @param {*} resourceOverrides
+   * @param {*} html
+   */
+  static disableDerivedResourceFields = (resourceOverrides, html) => {
+    const indexesToDisable = Object.keys(resourceOverrides).map(this.getIndexFromResourceName);
     const resourceElements = html.find('.resource');
 
     // get the resources which have overrides to disable inputs
-    indexesToDisable.forEach( index => {
+    indexesToDisable.forEach(index => {
       const element = resourceElements[index];
       // disable every input except the `value` input
-      $(element).find('[name]').filter((index, el) => !el.name.includes('value')).prop('disabled', true);
+      $(element).find('[name]').filter((index, el) => !el.name.includes('value'))
+        .prop('disabled', true)
+        .prop('title', game.i18n.localize(`${LinkItemResource5e.MODULE_NAME}.disabled-resource-helper-text`));
     })
   }
-}
 
-Hooks.on('renderActorSheet5eCharacter', LinkItemResource5eActorSheet.disableDerivedFields);
+  /**
+   * Disables the Item Uses inputs on the inventory level for items which override resource.
+   * This is necessary to avoid a double-update which results in no actual change.
+   * @param {*} resourceOverrides 
+   * @param {*} html 
+   * @returns 
+   */
+  static disableOverriddenItemFields = (resourceOverrides, html) => {
+    const itemIdsToDisable = Object.values(resourceOverrides);
+
+    // get the resources which have overrides to disable inputs
+    itemIdsToDisable.forEach(itemId => {
+      html.find(`[data-item-id=${itemId}] .item-uses input`)
+        .prop('disabled', true)
+        .prop('title', game.i18n.localize(`${LinkItemResource5e.MODULE_NAME}.disabled-item-helper-text`));
+    });
+  }
+}
